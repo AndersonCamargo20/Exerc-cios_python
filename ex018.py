fa01685dde1fa7c6236f2a789ce14325d80640df
Andeson de Camargo <anderson.camargo@unochapeco.edu.br>
2018-09-07 22:34:11 -0300

    more exercices

---
+++ ex018.py
@@ -0,0 +1,10 @@
+print('========== DESAFIO 018 ==========')
+print('ATRAVÉS DA LEITURA DO ÂNGULO CALCULE O SENO, COSSENO E A TANGENTE')
+
+from math import radians, cos, sin, tan
+angulo = float(input('Informe o Ângulo para o cálculo: '))
+sen = sin(radians(angulo))
+cos = cos(radians(angulo))
+tangente = tan(radians(angulo))
+
+print('O ângulo de {} tem o SENO de {} e o COSSENO de {}, que por sua vez gera a TANGENTE de {} !'.format(angulo, round(sen, 2), round(cos, 2), round(tangente, 2)))
